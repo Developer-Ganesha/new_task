f25f853482fe2055666c0dfe24ab98c06e159b86
Developer-Ganesha <ganeshsavner320@gmail.com>
2025-03-25 18:27:02 +0530

    added the code

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from jose import JWTError
 from datetime import datetime, timedelta
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 app = FastAPI()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -17,7 +17,6 @@ DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
